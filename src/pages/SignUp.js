5c738d38f26dce6b0ceb1459bbf2bd960da419de
ToshihiroAsami <inguumdct@gmail.com>
2021-06-15 20:57:04 +0900

    feat: put contents on SignUp page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import Layout from "../components/Layout";
 
+import {Form,Button, Container} from "react-bootstrap"
+
 export default class SignUp extends React.Component {
   render() {
     return (
-      <Layout>SignUp Page!</Layout>
+      <Layout>SignUp Page!
+        <Container>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" placeholder="Password Confirmation" />
+            </Form.Group>
+            {/* <Form.Select> なんかエラーでる*/}
+              <select>
+                <option>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            {/* </Form.Select> */}
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="利用規約に同意する" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
+      </Layout>
     );
   }
 }
