1a725e8afbfa142548c0df2a4a66de006f2305a7
keensyo <k66mdg55@gmail.com>
2021-08-10 17:55:55 +0900

    feat: add team_name in support_info

--- src/components/SupportInfo.js
+++ src/components/SupportInfo.js
@@ -9,8 +9,7 @@ const SupportInfo = ({support_info}) => {
       <Row>
         <Col>
           <Image className="emblem" src={`${process.env.PUBLIC_URL}/my_page_header.jpeg`} roundedCircle />
-          <h6>応援しているチーム</h6>
-          <h6>{JSON.stringify(support_info)}</h6>
+          <h6>{`応援しているチーム：${support_info.team_name}`}</h6>
         </Col>
         <Col>
           <Row><h6>{`${support_info.win}勝${support_info.lose}敗${support_info.draw}分`}</h6></Row>
