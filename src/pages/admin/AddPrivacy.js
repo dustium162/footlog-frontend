bca63234ce8fd7a27341d9d24e8b73403a407c69
ToshihiroAsami <inguumdct@gmail.com>
2021-08-25 21:23:34 +0900

    feat: Implemented edit terms and privacy function.

--- src/pages/admin/AddPrivacy.js
+++ src/pages/admin/AddPrivacy.js
@@ -6,18 +6,24 @@ import {InputGroup,FormControl,Button} from "react-bootstrap"
 
 const AddPrivacy = () => {
   const [privacies,setPrivacies] = useState("")
+
+  const handlePrivacy = (e) => {
+    setPrivacies(e.target.value)
+  }
+
   useEffect( () => {
     axios.get(`${process.env.REACT_APP_API_ENDPOINT}/privacies`)
     .then(response => response.data)
     .then(data => setPrivacies(data))
   },[])
   const updatePrivacies = () => {
+    axios.post(`${process.env.REACT_APP_API_ENDPOINT}/privacies`,{content: privacies})
   }
   return (
     <Layout>
       <InputGroup>
         <InputGroup.Text>プライバシーポリシー</InputGroup.Text>
-        <FormControl as="textarea" placeholder={privacies.text ? privacies.text :""} aria-label="With textarea" />
+        <FormControl as="textarea" defaultValue={privacies.text ? privacies.text :""} aria-label="With textarea" onChange={handlePrivacy}/>
         <Button variant="outline-secondary" id="button-addon2" onClick={updatePrivacies}>更新</Button>
       </InputGroup>
     </Layout>
