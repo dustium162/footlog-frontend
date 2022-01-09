f373ad9d404c91b1a129c6b067b78c25fbc0351c
amount86 <ry0.urag@gmail.com>
2022-01-09 22:42:37 +0900

    feat: add ads.txt

--- src/pages/InformRenewal.js
+++ src/pages/InformRenewal.js
@@ -8,7 +8,9 @@ const Privacy = () => {
       <h1 className="text-center">現在、サービスをリニューアルしております。</h1>
       <h2 className="text-center">投稿いただいておりますデータは削除されません。</h2>
       <h2 className="text-center">サービス再開まで、今しばらくお待ち下さい。</h2>
-      <AdsCard className="my-5" />
+      <div className="my-5">
+        <AdsCard />
+      </div>
     </Container>
   )
 }
