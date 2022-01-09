fcd6eba9106c0e9dac1d66270a23932197740fe9
amount86 <ry0.urag@gmail.com>
2022-01-09 20:16:59 +0900

    fix:fixed layout AdsCard.js.

--- src/pages/InformRenewal.js
+++ src/pages/InformRenewal.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import AdsCard from '../components/AdsCard';
 
 const Privacy = () => {
   return (
-    <div>
+    <Container>
       <h1 className="text-center">現在、サービスをリニューアルしております。</h1>
       <h2 className="text-center">投稿いただいておりますデータは削除されません。</h2>
       <h2 className="text-center">サービス再開まで、今しばらくお待ち下さい。</h2>
-      <AdsCard />
-    </div>
+      <AdsCard className="my-5" />
+    </Container>
   )
 }
 export default Privacy;
