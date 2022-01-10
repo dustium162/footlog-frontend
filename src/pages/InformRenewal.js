3f9ec697fed305d2cf7c57eb0ae64d36a62a2636
amount86 <ry0.urag@gmail.com>
2022-01-10 23:08:09 +0900

    fix: remove AdsCard from InformRenewal.

--- src/pages/InformRenewal.js
+++ src/pages/InformRenewal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import AdsCard from '../components/AdsCard';
+// import AdsCard from '../components/AdsCard';
 
 const Privacy = () => {
   return (
@@ -8,9 +8,9 @@ const Privacy = () => {
       <h1 className="text-center">現在、サービスをリニューアルしております。</h1>
       <h2 className="text-center">投稿いただいておりますデータは削除されません。</h2>
       <h2 className="text-center">サービス再開まで、今しばらくお待ち下さい。</h2>
-      <div className="my-5">
+      {/* <div className="my-5">
         <AdsCard />
-      </div>
+      </div> */}
     </Container>
   )
 }
