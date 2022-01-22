ce24d3f8a12e2a69149055fe78ab940ac1e31906
amount86 <ry0.urag@gmail.com>
2022-01-22 16:15:16 +0900

    fix: Fixed a bug that prevented some match information from being loaded.

--- src/pages/Matches.js
+++ src/pages/Matches.js
@@ -1,33 +1,35 @@
 import React,{useState,useEffect} from 'react';
-
 import InfiniteScroll from 'react-infinite-scroller'
 import Head from '../components/Head';
 import Layout from '../components/Layout';
-import axios from 'axios'
-import {Container, Spinner} from 'react-bootstrap'
-import MatchCard from '../components/MatchCard'
-
-import {TransitionMotion,spring} from 'react-motion'
+import axios from 'axios';
+import {Container, Spinner} from 'react-bootstrap';
+import MatchCard from '../components/MatchCard';
+import {TransitionMotion,spring} from 'react-motion';
 
 const Matches = () => {
-  const [matches,setMatches] = useState([])
-  const [hasMore,setHasMore] = useState(true)
+  const [matches, setMatches] = useState([]);
+  const [matchIds, setMatchIds] = useState([]);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
 
-  const loadMore = async (page) => {
-
-    const response = await axios(`${process.env.REACT_APP_API_ENDPOINT}/matches?page=${page}`, {
-      headers: {
-        uid: localStorage.getItem('uid'),
-        'access-token': localStorage.getItem('access-token'),
-        client: localStorage.getItem('client')
+  const loadMore = async () => {
+    console.log(matchIds)
+    if(matchIds.length !== 0){
+      const data = await getMatches();
+      console.log(data);
+      console.log(matchIds);
+      console.log(matches);
+      if (data.length < 10) {
+        setHasMore(false);
+        return;
       }
-    });
-    const data = response.data
-    if (data.length < 1) {
-      setHasMore(false);
-      return;
+      if(matches.length !== 0){
+        setMatches([...matches,...data])
+      } else {
+        setMatches(data);
+      }
     }
-    setMatches([...matches,...data])
   }
 
   const loader = <Spinner key={0} animation="border" variant="secondary" />
@@ -40,12 +42,29 @@ const Matches = () => {
       }
     })
     // apiのJsonの形式を検討する必要あり（2021-07-19 浦郷）
-    .then(response => response.data )
-    .then(data => setMatches(data))
-    }
-  ,[])
+    .then(response => response.data)
+    .then(data => {
+      setMatchIds(data);
+      return matchIds;
+    })
+  },[])
 
-  const willLeave = () => {
+  const getMatches = async () => {
+    console.log(matchIds[3]);
+    const response = await axios(`${process.env.REACT_APP_API_ENDPOINT}/matches/add?match_ids=${matchIds[page]}`, {
+      headers: {
+        uid: localStorage.getItem('uid'),
+        'access-token': localStorage.getItem('access-token'),
+        client: localStorage.getItem('client')
+      }
+    });
+    console.log(response.data);
+    setPage(() => page+1);
+    // console.log(response.data);
+    return response.data
+  }
+
+  const willLeave = () => { 
     return {height: spring(0,{stiffness:240,dumping:30})}
   }
 
@@ -61,10 +80,10 @@ const Matches = () => {
             <TransitionMotion
               willLeave={willLeave}
               styles={
-                matches.map((match,id) => (
+                matches.map((match) => (
                   {
                     key: String(match.match_id),
-                    data:{...match,id},
+                    data: match,
                     style:{height: match.height}
                   }
                 ))
