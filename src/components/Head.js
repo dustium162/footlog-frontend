faecffd2f604da0fdd4d9b53c05b2556ef47e1a9
amount86 <ry0.urag@gmail.com>
2022-03-19 10:01:21 +0900

    style: Corrected an error.

--- src/components/Head.js
+++ src/components/Head.js
@@ -10,12 +10,12 @@ const Head = (props) => {
       <title>{title}</title>
       <meta
           name="description"
-          content="footlogは、Jリーグサポートターのための観戦記録サービスです。"
+          content="footlogは、Jリーグサポーターのための観戦記録サービスです。"
       />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@footlog_j" />
       <meta name="twitter:title" content="footlog" />
-      <meta name="twitter:description" content="footlogは、Jリーグサポートターのための観戦記録サービスです。" />
+      <meta name="twitter:description" content="footlogは、Jリーグサポーターのための観戦記録サービスです。" />
       <meta name="twitter:image" content="https://footlog.net/" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0"></meta>
     </Helmet>
